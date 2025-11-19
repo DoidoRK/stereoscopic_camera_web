ffa7deb3a6aa359f4be831f9c316a87de94f785b
João Carlos Gobatto da Cunha <doidobr46@gmail.com>
2025-11-19 10:36:59 -0300

    New configurations made

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,23 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const leftImg = document.getElementById("leftImage") as HTMLImageElement;
+const rightImg = document.getElementById("rightImage") as HTMLImageElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+interface WSMessage {
+  type: "left" | "right";
+  image: string; // base64
+}
+
+const ws = new WebSocket("ws://localhost:3001");
+
+ws.onmessage = event => {
+  const data: WSMessage = JSON.parse(event.data);
+
+  const src = `data:image/jpeg;base64,${data.image}`;
+
+  if (data.type === "left") {
+    leftImg.src = src;
+  } else if (data.type === "right") {
+    rightImg.src = src;
+  }
+};
